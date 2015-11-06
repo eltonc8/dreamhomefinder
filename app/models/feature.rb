f0f337e6d0fd53e19bdd3cdff7cd8f05af82d359
eltonc88 <eltonchan@cal.berkeley.edu>
2015-11-06 12:47:49 -0800

    add feature validations

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,8 @@
 class Feature < ActiveRecord::Base
+  validates_inclusion_of :lat, in: -90..90
+  validates_inclusion_of :lng, in: -180..180
+  validates :lat, :lng, numericality: true
+  validates :bedrooms, :bathrooms, :price, :sq_ft,
+            numericality: { only_integer: true }
+  validates :identity, :status, :street, presence: true
 end
