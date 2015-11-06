6df36e14b8846b171c271e5f31f1ce9c10f759c5
eltonc88 <eltonchan@cal.berkeley.edu>
2015-11-06 12:35:50 -0800

    add seed data and sampel json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seed_data = IO.readlines("db/listings.csv")
+data_header = seed_data.shift.split(/[\n,]/)
+data_header[0] = "identity" if data_header[0] == "id"
+
+seed_data.each do |line|
+  data = line.split(/[\n,]/)
+  attributes = {}
+  data_header.length.times do |idx|
+    attributes[ data_header[idx] ] = data[idx]
+  end
+  Feature.create!( attributes )
+end
